afb7063f1e6ef76c16377a013ebc63f7f2728355
Wocli <woclidev@outlook.com>
2022-04-27 11:03:15 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-5jsrjf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-5jsrjf)
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+import Nav from './components/Nav';
+import Main from './components/Main';
+
+export default function App() {
+  return (
+    <div>
+      <Nav />
+      <Main />
+    </div>
+  );
+}
